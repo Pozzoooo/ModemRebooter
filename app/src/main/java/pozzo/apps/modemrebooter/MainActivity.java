31c275ae82fe8e6e88f8943991fadbadddfdb9fa
Luiz Gustavo Pozzo <lgppozzo@hotmail.com>
2016-01-19 22:14:54 -0200

    Basic structure created.

--- app/src/main/java/pozzo/apps/modemrebooter/MainActivity.java
+++ app/src/main/java/pozzo/apps/modemrebooter/MainActivity.java
@@ -1,69 +1,105 @@
 package pozzo.apps.modemrebooter;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.util.ArrayList;
+
+/**
+ * Basicaly everything =].
+ */
 public class MainActivity extends AppCompatActivity {
+	private WebView webView;
+	private LoaderQueue loaderQueue;
+
+	/**
+	 * This is what is going on.
+	 */
+	private void script() {
+		String url = "http://192.168.1.1/";
+
+		loaderQueue.add(url)//Load the entry page
+				//Click on login
+				.add("javascript:document.getElementsByName(\"login\")[0].click();")
+				//Reboot page
+				.add(url + "tools_system.htm")
+				.add("restart");
+
+		loaderQueue.start();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+		setContentView(R.layout.content_main);
 
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
-
-		loadWebView();
+		setupWebview();
+		script();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-
-		if(id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
-	private void loadWebView() {
-		final WebView webView = (WebView) findViewById(R.id.webView);
+	/**
+	 * Create the webview.
+	 */
+	private void setupWebview() {
+		webView = (WebView) findViewById(R.id.webView);
 		WebSettings settings = webView.getSettings();
 		settings.setJavaScriptEnabled(true);
-
-		webView.loadUrl("http://192.168.1.1/");
-		webView.setWebViewClient(new WebViewClient(){
-			@Override
-			public void onPageFinished(WebView view, String url) {
-				login(webView);
-			}
-		});
+		loaderQueue = new LoaderQueue();
+		webView.setWebViewClient(loaderQueue);
 	}
 
-	private void login(WebView webView) {
-		webView.loadUrl("javascript:document.getElementsByName(\"login\")[0].click();");
+	/**
+	 * Loads a page and request something to run after loaded.
+	 *
+	 * @param toLoad To be loaded now,
+	 * @param afterLoad To be loaded after fully loaded.
+	 */
+	private void load(String toLoad, WebViewClient afterLoad) {
+		webView.loadUrl(toLoad);
+		webView.setWebViewClient(afterLoad);
+	}
+
+	/**
+	 * Chain page loads.
+	 * Make sure to start the execution with #start().
+	 */
+	private class LoaderQueue extends WebViewClient {
+		private ArrayList<String> tasks = new ArrayList<>();
+
+		/**
+		 * Add an entry to the execution queue.
+		 */
+		public LoaderQueue add(String task) {
+			tasks.add(task);
+			return this;
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+			executeSingleValidCommand();
+		}
+
+		/**
+		 * Start executing tasks.
+		 * They will chain if they all load things.
+		 */
+		public void start() {
+			executeSingleValidCommand();
+		}
+
+		private void executeSingleValidCommand() {
+			//Make sure it is not empty and there is no null element
+			while(!tasks.isEmpty()) {
+				String next = tasks.remove(0);
+
+				if(next != null) {
+					webView.loadUrl(next);
+					break;
+				}
+			}
+		}
 	}
 }
